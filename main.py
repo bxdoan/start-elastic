b4b08af5494aa3fb35d349fef86571c99bc5018b
Doan Bui <bxdoan93@gmail.com>
2024-01-16 14:56:38 +0700

    first

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+import random
+
+logging.basicConfig(filename="logFile.txt",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+for i in xrange(0,15):
+    x=random.randint(0,2)
+    if(x==0):
+        logging.warning('Log Message')
+    elif(x==1):
+        logging.critical('Log Message')
+    else:
+        logging.error('Log Message')
